f8134ae79e8bb27a2df02812bdb1eec5b37a8e7b
Vladimir Bogretsov <bogrecov@gmail.com>
2025-03-18 23:01:40 +0300

    Added lsmt

--- bin/lsmt.sh
+++ bin/lsmt.sh
@@ -3,7 +3,8 @@
 # Function to display usage
 usage() {
     echo "Usage: $0 -t <thread_range> -c <connection_range> -R <request_rate_range> -d <duration> -L <url>"
-    echo "Example: $0 -t 1-4 -c 10-100 -R 1000-5000 -d 120s -L http://localhost/"
+    echo "  Ranges should be in the format: start-end-step"
+    echo "Example: $0 -t 1-4-1 -c 10-100-10 -R 1000-5000-1000 -d 120s -L http://localhost/"
     exit 1
 }
 
@@ -30,12 +31,12 @@ if [ -z "$THREAD_RANGE" ] || [ -z "$CONNECTION_RANGE" ] || [ -z "$REQUEST_RATE_R
     usage
 fi
 
-# Function to generate a range of numbers
+# Function to generate a range of numbers with a step
 generate_range() {
     local range=$1
-    local start end
-    IFS='-' read -r start end <<< "$range"
-    seq $start $end
+    local start end step
+    IFS='-' read -r start end step <<< "$range"
+    seq "$start" "$step" "$end"
 }
 
 # Generate ranges for threads, connections, and request rates
@@ -48,7 +49,7 @@ for t in "${THREADS[@]}"; do
     for c in "${CONNECTIONS[@]}"; do
         for R in "${REQUEST_RATES[@]}"; do
             echo "Running wrk with -t$t -c$c -R$R -d$DURATION -L$URL"
-            /local/wrk -t$t -c$c -d$DURATION -R$R -L "$URL"
+            /local/wrk -t"$t" -c"$c" -d"$DURATION" -R"$R" -L "$URL"
             echo "---------------------------------------------"
         done
     done
